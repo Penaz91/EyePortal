a537b19233bc2fb514558c5dd96480f6ffab98c6
Penaz91 <penazarea@altervista.org>
2016-10-29 12:44:25 +0200

    Added Item-based limit

---
+++ src/main/java/eyePortal/ItemizedTeleportListener.java
@@ -0,0 +1,21 @@
+package eyePortal;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.Inventory;
+
+public class ItemizedTeleportListener implements Listener{
+	@EventHandler
+	public void OnTeleport (PlayerTeleportEvent event){
+		if (event.getCause() == PlayerTeleportEvent.TeleportCause.END_PORTAL){	
+			int num = Main.cfg.getInt("quantity");
+			Inventory invo = event.getPlayer().getInventory();
+			if (invo.containsAtLeast(Main.it, num)){
+				invo.removeItem(Main.it);
+			}else{
+				event.setCancelled(true);
+			}
+		}
+	}
+}
--- src/main/java/eyePortal/Main.java
+++ src/main/java/eyePortal/Main.java
@@ -1,16 +1,24 @@
 package eyePortal;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Map;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public class Main extends JavaPlugin{
 	public static Configuration cfg = null;
+	public static ItemStack it = null;
 	@Override
 	public void onEnable(){
 		File f = getDataFolder();
@@ -19,7 +27,14 @@ public class Main extends JavaPlugin{
 			saveResource("config.yml", false);
 		}
 		cfg = this.getConfig();
-		getServer().getPluginManager().registerEvents(new TeleportListener(), this);
+		if (cfg.getString("block").equalsIgnoreCase("###ITEM###")){
+			File customfile = new File(getDataFolder(),"item.yml");
+			FileConfiguration customyml=YamlConfiguration.loadConfiguration(customfile);
+			it=customyml.getItemStack("item");
+			getServer().getPluginManager().registerEvents(new ItemizedTeleportListener(), this);
+		}else{
+			getServer().getPluginManager().registerEvents(new TeleportListener(), this);
+		}
 		getLogger().info("Plugin loaded");
 	}
 	@Override
@@ -34,6 +49,42 @@ public class Main extends JavaPlugin{
 			getLogger().info("Configuration reloaded");
 			sender.sendMessage("Configuration reloaded");
 			return true;
+		}else{
+			if (cmd.getName().equalsIgnoreCase("epsetitem")){
+				if (sender instanceof Player){
+					ItemStack i=((Player) sender).getInventory().getItemInMainHand();
+					File customfile = new File(getDataFolder(),"item.yml");
+					FileConfiguration customyml=YamlConfiguration.loadConfiguration(customfile);
+					Map<String, Object> map=customyml.getValues(false);
+					if (map.get("item")==null){
+						customyml.createSection("item");
+						customyml.set("item",i);
+					}else{
+						customyml.set("item", i);
+					}
+					try {
+						customyml.save(customfile);
+					} catch (IOException e) {
+						sender.sendMessage("Unable to save item, item.yml not found");
+					}
+				}else{
+					sender.sendMessage("This command can be executed only by a player");
+				}
+				return true;
+			}else{
+				if (cmd.getName().equalsIgnoreCase("epgetitem")){
+					PlayerInventory pi = ((Player) sender).getInventory();
+					if (pi.firstEmpty()==-1) {
+						sender.sendMessage("You need at least 1 free space in your inventory");
+					}else{
+						File customfile = new File(getDataFolder(),"item.yml");
+						FileConfiguration customyml=YamlConfiguration.loadConfiguration(customfile);
+						ItemStack i=customyml.getItemStack("item");
+						pi.addItem(i);
+					}
+					return true;
+				}
+			}
 		}
 		return false;}
 }
